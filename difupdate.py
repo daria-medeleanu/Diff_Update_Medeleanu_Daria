0835728b43aea444f54e04a7be49679255d09085
daria-medeleanu <medeleanudaria14@gmail.com>
2024-12-13 12:44:13 +0200

    recognition-of-commands
    The only valid commands are update and create since we can't go to a previous version of the documents

--- difupdate.py
+++ difupdate.py
@@ -1,12 +1,27 @@
+import sys
+
 file_path = "./file.bin"
+valid_commands = ['update', 'create']
+if len(sys.argv) < 4:
+    print('Invalid number of arguments')
+    sys.exit(1)
 
-with open(file_path,  "wb") as file:
-    binary_data = b"salut"
-    file.write(binary_data)
+try:
 
-with open(file_path, 'rb') as file:
-    fileContent = file.read()
-    print(fileContent)
+    command = sys.argv[1]
+    if command not in valid_commands:
+        raise ValueError('The command is not valid. The only accepted commands are: create and update')
+
+    with open(file_path,  "wb") as file:
+        binary_data = "salut"
+        file.write(binary_data.encode())
+
+    with open(file_path, 'rb') as file:
+        fileContent = file.read()
+        print(fileContent.decode())
+except ValueError as ve:
+    print(f"Error: {ve}")
+
 
 
 
