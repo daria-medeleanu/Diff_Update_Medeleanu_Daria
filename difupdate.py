f401016a6c2fc88a69d55da0138aab32cea2e743
daria-medeleanu <medeleanudaria14@gmail.com>
2025-01-10 21:42:17 +0200

    comparison

--- difupdate.py
+++ difupdate.py
@@ -9,8 +9,28 @@ def create_diff_file(latest_file, current_file, diff_file_path):
             with open(current_file, 'rb') as current:
                 latest_data = latest.readlines()
                 current_data = current.readlines()
-                for l in latest_data:
-                    print(l)
+                max_lines = max(len(latest_data), len(current_data))
+                
+                for i in range(max_lines):
+                    if(i==len(latest_data) or i==len(current_data)):
+                        break
+                    if len(current_data[i]) < len(latest_data[i]):
+                        print('line ', i+1,' insert ',latest_data[i])
+                    elif(len(current_data[i]) == len(latest_data[i]) and current_data[i] != latest_data[i]):
+                        print('line ', i+1, ' update - ', current_data[i])
+                        print('line ', i+1, ' update + ', latest_data[i])
+                    elif(len(current_data[i]) > len(latest_data[i])):
+                        print('line ', i+1, ' delete',current_data[i])
+                        print('line ', i+1, ' insert',latest_data[i])
+                print(i, len(latest_data), len(current_data))
+                if (i <len(current_data)):
+                    for j in range(i, len(current_data)):
+                        if len(current_data) > len(latest_data):
+                            print('line ', j+1, ' delete' ,current_data[j])
+                if( i< len(latest_data)):
+                    for j in range(i, len(latest_data)):
+                        if len(current_data) < len(latest_data):
+                            print('line ', j+1, ' insert',latest_data[j])
 
 
 if len(sys.argv) < 4:
@@ -27,7 +47,7 @@ try:
         case "create":
             latest_file = sys.argv[2]
             diff_file_path = "./files/diff_file.bin"
-            for current_file in sys.argv[3:]:
+            for current_file in sys.argv[5:]:
                 create_diff_file(latest_file, current_file, diff_file_path)
         case "update":
             print('you used update command')
