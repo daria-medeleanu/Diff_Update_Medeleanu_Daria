b294c15a2593a73fff13dc09dbcabbed7b268397
daria-medeleanu <medeleanudaria14@gmail.com>
2025-01-14 23:26:00 +0200

    generate_command
    added PEP8

--- difupdate.py
+++ difupdate.py
@@ -1,70 +1,125 @@
 import sys
+import os
 
-file_path = "./files/file_latest.bin"
-valid_commands = ['update', 'create']
+VALID_COMMANDS = ['update', 'create']
 
-def create_diff_file(latest_file, current_file, diff_file_path):
+
+def create_diff_file(latest_file, current_file_path, diff_file_path):
     with open(diff_file_path, 'ab') as diff_file:
-        diff_file.write(b'\ndifference-for-'+current_file.encode()+b'\n')
+        diff_file.write(b'\ndifference-for-'+current_file.encode("utf-8")+b'\n')
         with open(latest_file, 'rb') as latest:
             with open(current_file, 'rb') as current:
                 latest_data = latest.readlines()
                 current_data = current.readlines()
-                max_lines = max(len(latest_data), len(current_data))
-                
-                for i in range(max_lines):
-                    if(i==len(latest_data) or i==len(current_data)):
-                        break
+                nr_min_lines = min(len(latest_data), len(current_data))
+                for i in range(nr_min_lines):
                     if len(current_data[i]) < len(latest_data[i]):
-                        diff_file.write(b'line '+str(i+1).encode()+b' insert '+latest_data[i])
-                    elif(len(current_data[i]) == len(latest_data[i]) and current_data[i] != latest_data[i]):
-                        diff_file.write(b'line '+str(i+1).encode()+ b' update '+ current_data[i])
-                        diff_file.write(b'line '+ str(i+1).encode()+ b' update '+ latest_data[i])
+                        diff_file.write(b'line '+str(i+1).encode("utf-8")+b' insert '+latest_data[i]+b'\n')
+                    elif(len(current_data[i]) == len(latest_data[i]) and current_data[i] != latest_data[i]+b'\n'):
+                        diff_file.write(b'line '+str(i+1).encode("utf-8")+ b' update '+ current_data[i]+b'\n')
+                        diff_file.write(b'line '+ str(i+1).encode("utf-8")+ b' update '+ latest_data[i]+b'\n')
                     elif(len(current_data[i]) > len(latest_data[i])):
-                        diff_file.write(b'line '+ str(i+1).encode()+ b' delete '+current_data[i])
-                        diff_file.write(b'line '+ str(i+1).encode()+ b' insert '+latest_data[i])
-
+                        diff_file.write(b'line '+ str(i+1).encode("utf-8")+ b' delete '+current_data[i]+b'\n')
+                        diff_file.write(b'line '+ str(i+1).encode("utf-8")+ b' insert '+latest_data[i]+ b'\n')
+                
                 if (i <len(current_data)):
                     for j in range(i, len(current_data)):
-                        if len(current_data) > len(latest_data):
-                            diff_file.write(b'line '+ str(j+1).encode()+ b' delete ' +current_data[j])
+                            diff_file.write(b'line '+ str(j+1).encode("utf-8")+ b' delete ' +current_data[j]+b'\n')
+                
                 if( i< len(latest_data)):
                     for j in range(i, len(latest_data)):
-                        if len(current_data) < len(latest_data):
-                            diff_file.write(b'line '+ str(j+1).encode()+ b' insert '+latest_data[j])
+                            diff_file.write(b'line '+ str(j+1).encode("utf-8")+ b' insert '+latest_data[j]+ b'\n')
 
 def generate_latest_file(current_file_path, diff_file_path):
-    new_latest_file_path = './files/new_latest_file.bin'
+    new_latest_file_path = './binary_files/new_latest_file.bin'
+
     with open(new_latest_file_path, 'wb') as new_latest_file:
         with open(diff_file_path, 'rb') as diff_file:
             diff_data = diff_file.readlines()
             with open(current_file_path, 'rb') as current_file:
                 current_data = current_file.readlines()
+                current_lines = {i : line for i, line in enumerate(current_data, start=1)}
+                file_name_current = os.path.splitext(os.path.basename(current_file_path))[0]+'.txt'+'\n'
+                
+                flag = False
+                for linie in diff_data:
+                    if linie.startswith(b'difference-for-') and linie[15:] == file_name_current.encode("utf-8"):
+                        flag = True
+                        continue
+                    elif linie.startswith(b'difference-for-') and linie[15:] != file_name_current.encode("utf-8"):
+                        flag = False
+                    if flag == True and not linie.startswith(b'\n'):
+                        linie_split = linie.split(b' ')
+                        line_num = linie_split[1]
+                        action = linie_split[2]
+                        content = b' '.join(linie_split[3:])
+                        line_num = int(line_num)
+                        if action == b'insert':
+                            current_lines[line_num] = content 
+                        elif action == b'delete':
+                            if line_num in current_lines:
+                                del current_lines[line_num]
+                        elif action == b'update':
+                                current_lines[line_num] = content 
+                
+                for i in sorted(current_lines.keys()):
+                    new_latest_file.write(current_lines[i])
             
-                            
-                    
+def delete_content_of_binary_file(file_path):
+    with open(file_path, 'wb') as file:
+        pass
+
 if len(sys.argv) < 4:
     print('Invalid number of arguments')
     sys.exit(1)
 
-try:
+def text_to_binary_file(text_file_path, binary_file_path):
+    with open(text_file_path, "r", encoding="utf-8") as text_file:
+        content = text_file.read()
 
+    with open(binary_file_path, "wb") as binary_file:
+        binary_file.write(content.encode("utf-8"))
+
+def binary_file_to_text(binary_file_path, text_file_path):
+    with open(binary_file_path, "rb") as binary_file:
+        content = binary_file.read().decode("utf-8")
+
+    with open(text_file_path, "w", encoding="utf-8") as text_file:
+        text_file.write(content)
+
+def transformVersonFilesIntoBinaryFiles(version_files):
+    for file in version_files:
+        binary_file_path = 'binary_files/'+os.path.splitext(file)[0]+'.bin'
+        text_to_binary_file(file, binary_file_path)
+
+try:
     command = sys.argv[1]
-    if command not in valid_commands:
+    if command not in VALID_COMMANDS:
         raise ValueError('The command is not valid. The only accepted commands are: create and update')
 
     match command:
         case "create":
             latest_file = sys.argv[2]
-            diff_file_path = "./files/diff_file.bin"
-            for current_file in sys.argv[3:]:
-                create_diff_file(latest_file, current_file, diff_file_path)
+            version_files = sys.argv[3:]
+
+            transformVersonFilesIntoBinaryFiles(version_files)            
+            latest_binary_file_path = 'binary_files/'+os.path.splitext(latest_file)[0]+'.bin'
+            text_to_binary_file(latest_file, latest_binary_file_path)
+
+            diff_file_path = "./binary_files/diff_file.bin"
+            delete_content_of_binary_file(diff_file_path)
+
+            for current_file in version_files:
+                binary_file_path = 'binary_files/'+os.path.splitext(current_file)[0]+'.bin'
+                create_diff_file(latest_binary_file_path, binary_file_path, diff_file_path)
+        
         case "update":
             current_file_path = sys.argv[2]
             diff_file_path = sys.argv[3]
             generate_latest_file(current_file_path, diff_file_path)
+            binary_file_to_text("binary_files/new_latest_file.bin","new_latest_file.txt")
+
 except ValueError as ve:
     print(f"Error: {ve}")
 except Exception as e:
     print(f"An expected error occured: {e}")
-    
